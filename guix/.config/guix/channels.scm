7a4b2f350cfe0e835915e4a353ac3cf8e599b786
Benoit Martin <benoitmartin88.pro@gmail.com>
2024-06-10 09:48:38 +0200

    add guix

---
+++ guix/.config/guix/channels.scm
@@ -0,0 +1,9 @@
+(append
+  (list
+    (channel
+      (name 'guix-hpc)
+      (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git"))
+    (channel
+      (name 'guix-hpc-non-free)
+      (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free.git")))
+   %default-channels)
