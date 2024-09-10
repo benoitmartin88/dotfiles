921c344b273bcf6244190621fe52655d9bda54cc
Benoit Martin <benoitmartin88.pro@gmail.com>
2024-09-10 11:46:38 +0200

    add guix-science-non-free channel

--- guix/.config/guix/channels.scm
+++ guix/.config/guix/channels.scm
@@ -5,5 +5,13 @@
       (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git"))
     (channel
       (name 'guix-hpc-non-free)
-      (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free.git")))
+      (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free.git"))
+    (channel
+      (name 'guix-science-nonfree)
+      (url "https://github.com/guix-science/guix-science-nonfree.git")
+      (introduction
+        (make-channel-introduction
+          "58661b110325fd5d9b40e6f0177cc486a615817e"
+          (openpgp-fingerprint
+          "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446")))))
    %default-channels)
